3601568232f4e2040a870cda3c13513413824a2e
Simon Bourk <sbourk@gmail.com>
2020-01-20 22:42:12 -0500

    initial commit

---
+++ Model/Company.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Invoices.Model
+{
+	public class Company : IComparable
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public Address Address { get; set; }
+		public string TradeRegisterNumber { get; set; }
+		public string FiscalCode { get; set; }
+
+		public Company()
+		{
+			Address = new Address();
+		}
+
+		public override string ToString()
+		{
+			return Name;
+		}
+
+		public int CompareTo(object other)
+		{
+			return String.Compare(ToString(), other.ToString());
+		}
+	}
+}
---
+++ Model/Invoice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Invoices.Model
+{
+	public class Invoice
+	{
+		public int Id { get; set; }
+		public string Series { get; set; }
+		public long Number { get; set; }
+		public DateTime Date { get; set; }
+		public int NIR { get; set; }
+		public int PV { get; set; }
+		public Contract Contract { get; set; }
+		public string Description { get; set; }
+		public decimal InvoicedValueInclusive { get; set; }
+		public decimal InvoicedValueExclusive { get; set; }
+
+		public Invoice()
+		{
+			Date = DateTime.Now;
+		}
+	}
+}
